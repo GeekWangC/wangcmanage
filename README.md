07713cea96b904fac2342325a22bad9ff17d5976
王超 <geekwangc@163.com>
2022-02-19 15:03:42 +0800

    '项目初始化'

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # wangcmanage
 个人博客后管系统
+
+
+npm init 初始化项目
+
+npm i express -D 
+npm i mysql -D
+
+https://www.scarsu.com/
+https://www.bmpi.dev/goal/
+https://blog.othing.xyz/
+http://gtdstudy.com/page/2/
+https://www.cnfeat.com/
+
+https://zhuanlan.zhihu.com/p/161928960
+https://leetcode-cn.com/problems/3u1WK4/
+
+https://www.bilibili.com/video/BV1Vy4y1z7EX?p=4&spm_id_from=pageDriver
+
+https://www.bilibili.com/video/BV1Rz4y1m7pt?p=4&spm_id_from=pageDriver
---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const util = require('./util.js')
+
+const app = express();
+
+
+
+// 发送get请求
+app.get('/',(req, resp)=>{
+	
+	util.read('pages/index.html')
+	.then(res=>{
+		resp.write(res)
+		resp.end();
+	})
+	// resp.write('<h1>hello node</h1>')
+	// resp.end();
+})
+
+app.get('/login',async (req, resp)=>{
+	const data = await util.read('pages/login.html')
+	resp.end(data);
+})
+
+app.get('/getUser',(req,resp)=>{
+	const pool = mysql.createPool({
+		database:'blog',
+		user:'root',
+		password:'password'
+	})
+	pool.getConnection((err,conn)=>{
+		if(!err){
+			const sql = 'select * from t_user';
+			const sqlParams = [];
+			conn.query(sql,sqlParams,(e,results)=>{
+				if(!e){
+					console.log(results)
+					resp.send(results);
+				}
+			})
+		}else{
+			console.log('err:',err)
+		}
+		
+	})
+})
+
+// 监听服务器端口
+app.listen(3000,()=>{
+	console.log('server is start');
+})
